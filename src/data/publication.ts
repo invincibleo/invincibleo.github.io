d8acc9d8286584ce70b3458335a51f1a09ab15ce
invincibleo <invincibleo@Duoweis-MacBook-Pro.local>
2025-05-17 11:47:00 +0200

    add one publication

--- src/data/publication.ts
+++ src/data/publication.ts
@@ -14,17 +14,17 @@ export interface Publication {
 export const publicationData: Publication[] = [
   // If you don't want to show publications, just make the array empty.
   {
-    year: "2024",
-    conference: "NeurIPS",
-    title: "Scalable Causal Discovery in High-Dimensional Time Series",
-    authors: "Jane Smith, Sarah Johnson, Yue Zhang",
-    paperUrl: "https://arxiv.org/abs/2409.15476",
-    codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
+    year: "2019",
+    conference: "IEEE Workshop on Applications of Signal Processing to Audio and Acoustics (WASPAA)",
+    title: "Supervised Contrastive Embeddings for Binaural Source Localization",
+    authors: "Duowei Tang, Maja Taseska, and Toon van Waterschoot",
+    paperUrl: "https://lirias.kuleuven.be/retrieve/603070",
+    //codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
     //bibtex: "https://arxiv.org/abs/2409.15476.bib",
     tldr: "Using causal discovery to find the causal structure of high-dimensional time series data.",
     imageUrl:
-      "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    award: "🏆 Best Paper Award",
+      "/pic/waspaa2019_SCE.png",
+    // award: "🏆 Best Paper Award",
     // if you have an image in public/images, you can use it like this:
     // imageUrl: "/images/publication-image.jpg"
   },
