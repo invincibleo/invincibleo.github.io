4a561456c58e01b82abd1838717289c9ddc1d81b
invincibleo <invincibleo@Duoweis-MacBook-Pro.local>
2025-05-17 11:39:44 +0200

    adjust gap to 0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       {/* Don't have a great call on whether max-w-screen-xl is better */}
       <div className="max-w-screen-lg mx-auto px-8 py-24">
         {/* Grid Layout */}
-        <div className="grid grid-cols-1 md:grid-cols-12 gap-1">
+        <div className="grid grid-cols-1 md:grid-cols-12 gap-0">
           {/* Left Column - Fixed Info */}
           <div className="col-span-12 md:col-span-4 space-y-12 mb-8 md:mb-0">
             {/* Profile */}
