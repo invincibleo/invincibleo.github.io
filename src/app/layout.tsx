3d0adcdc379459a90189ea7e687306aaabeb0540
invincibleo <invincibleo@Duoweis-MacBook-Pro.local>
2025-05-17 11:27:41 +0200

    change email at bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
                   href="mailto:duowei.tang@kuleuven.be"
                   className="underline hover:text-neutral-800 dark:hover:text-neutral-300 transition-colors"
                 >
-                  research-website-template
+                  duowei.tang[at]kuleuven.be
                 </a>
               </p>
             </div>
