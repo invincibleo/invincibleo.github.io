ad81ebf5081e8f7a17b8ca0c62b8322f2dc264ae
invincibleo <invincibleo@Duoweis-MacBook-Pro.local>
2025-06-08 14:04:44 +0200

    Correct about me

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -23,7 +23,7 @@ export const aboutMe: AboutMe = {
   institution: "Goodix / KU Leuven",
   // Note that links work in the description
   description:
-    "I'm currently an AI and speech algorithm designer at Goodix Technology. From 2017 to 2021, I had been a doctoral researcher doctoral researcher at the Electrical Engineering Department (ESAT), in the research group of Stadius Center for Dynamical Systems, Signal Processing, and Data Analytics (STADIUS) under the supervision of Prof. Toon van Watershoot, Prof. Luc Geurts and Prof. Peter Kuppens, in KU Leuven, Belgium. In 2015 and 2016, I received two M.Sc. degrees in Electronics Engineering and in Artificial Intelligence, respectively, from KU Leuven, Belgium. My research is focusing on speech and audio processing with deep learning, speech emotion recognition, real-time deep learning for edge devices.",
+    "I'm currently an AI and speech algorithm designer at Goodix Technology. From 2017 to 2021, I had been a doctoral researcher at the Electrical Engineering Department (ESAT), in the research group of Stadius Center for Dynamical Systems, Signal Processing, and Data Analytics (STADIUS) under the supervision of Prof. Toon van Watershoot, Prof. Luc Geurts and Prof. Peter Kuppens, in KU Leuven, Belgium. In 2015 and 2016, I received two M.Sc. degrees in Electronics Engineering and in Artificial Intelligence, respectively, from KU Leuven, Belgium. My research is focusing on speech and audio processing with deep learning, speech emotion recognition, real-time deep learning for edge devices.",
   email: "duowei.tang[at]kuleuven.be",
   imageUrl:
     "/pic/duowei_photo.png",
